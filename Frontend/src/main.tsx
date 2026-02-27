5c1bbceff638a4c0bfdb9119ec952c0119b81b38
geda <gediizeyuu@gmail.com>
2026-02-27 13:39:21 +0300

    fix speed insight next to react

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { ThemeProvider } from './components/ThemeProvider'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
